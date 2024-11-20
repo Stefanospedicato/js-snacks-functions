c9d7980edd8fbddcb99f53b2b3c344e1c7437942
Stefanospedicato <stefano.spedicato11@gmail.com>
2024-11-20 14:41:51 +0100

    feat: conta vocali complete

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,25 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+/*
+1.Creo una funzione.
+2.Devo avere un contatore che mi si incrementa quando incontra una vocale.
+*/
+
+function numeroVocali(word){
+  let contatore = 0
+  const vocali = ['a','e','i','o','u']
+  for(let i = 0; i < word.length; i++ ){
+    if(vocali.includes(word[i]))
+      contatore++
+  }
+  return contatore
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const numeroDiVocali = (numeroVocali(word))
+console.log(numeroDiVocali)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
