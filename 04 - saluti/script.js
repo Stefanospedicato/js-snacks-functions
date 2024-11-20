da3bda7bce888a9c053a1122638a82eced1570c4
Stefanospedicato <stefano.spedicato11@gmail.com>
2024-11-20 14:47:58 +0100

    feat: saluti complete

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,18 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+/*
+1. Creo una funzione che accetta una stringa
+2. Devo concatenare la mia stringa ad un testo
+*/
 
+function saluto(word){
+  return 'Ciao ' + word
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const salutoNome = saluto(name);
+console.log(salutoNome)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
