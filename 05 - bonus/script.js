3bb2b1526b2fd1cd4303e79abb4d46e6888a05dd
Stefanospedicato <stefano.spedicato11@gmail.com>
2024-11-20 14:57:29 +0100

    feat: bonus complete

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -9,10 +9,29 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+/*
+1.Creo una funzione che accetti una sola stringa
+2. Creo la variabile per l'ora e in base all'ora do' i messaggi differenti
+*/
+
+function salutoGiorno(word){
+  let message;
+  const date = new Date()
+  const hour = date.getHours()
+  if(hour<14){
+    message = 'Buongiorno '
+  } else if(hour>17){
+    message = 'Buonasera '
+  } else{
+    message = 'Buon pomeriggio '
+  }
+  return message + word
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const saluto = salutoGiorno(name);
+console.log(saluto)
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
